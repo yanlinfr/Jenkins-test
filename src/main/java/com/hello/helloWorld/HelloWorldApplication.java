94536e056b87594bc478d25fa12c021874b36621
yanestl <estelle_lin@hotmail.com>
2022-08-29 15:20:45 +0200

    project 2

--- src/main/java/com/hello/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/hello/helloWorld/HelloWorldApplication.java
@@ -1,7 +1,5 @@
 package com.hello.helloWorld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 public class HelloWorldApplication {
