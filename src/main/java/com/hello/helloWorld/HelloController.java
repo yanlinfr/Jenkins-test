0e1c5563250c972fb85631ba2e4ca38096e08d8d
yanestl <estelle_lin@hotmail.com>
2022-08-29 14:46:40 +0200

    init project

---
+++ src/main/java/com/hello/helloWorld/HelloController.java
@@ -0,0 +1,13 @@
+package com.hello.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return "Hello world";
+    }
+}
