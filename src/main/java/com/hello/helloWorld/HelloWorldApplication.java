d09d8c01964e93d271435e54e6d999e0866e7165
yanlinfr <71259741+yanlinfr@users.noreply.github.com>
2022-08-29 15:29:48 +0200

    Update HelloWorldApplication.java

--- src/main/java/com/hello/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/hello/helloWorld/HelloWorldApplication.java
@@ -5,7 +5,7 @@ package com.hello.helloWorld;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello cutie!");
 	}
 
 }
